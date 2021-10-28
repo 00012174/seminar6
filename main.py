1ab8e87f4b69e57d3fe36c8214f7c365edf00298
00012174wiutstudent <00012174wiutstudent@gmail.com>
2021-10-28 12:07:37 +0500

    cum ment

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+def test_is_valid(test):
+    if (isinstance(test, int)==True) and (1<= test <=3):
+        return True
+    else:
+        return False
+print(test_is_valid(2))
+print(test_is_valid("ryhrh"))
+print(test_is_valid(7))
 
